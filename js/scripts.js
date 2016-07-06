2d84fa2f1e3c3d71faa70275b4741b6f1c6c1e25
Joel Waage <joeltwaage@gmail.com>
2016-07-06 11:23:06 -0700

    refactor business logic, add separate answers for each of the three buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,44 @@
 // Business Logic
+var obiAnswer = ["It is decidedly so","Yes","You may rely on it","Very doubtful","The Force is hazy, ask again later"]
+var darthAnswer = ["My sources say No","Don't Count on it","Without a doubt","Don't ask me again"]
+var yodaAnswer = ["Ask again, you must","Doubtful, it is","Definitely","Certain, it is","Tell you now, I cannot"]
 
 
-function randomNumber() {
-  var number = Math.floor((Math.random() *20) + 1);
-  var prediction;
-  if (number === 1) {
-    prediction = "It is certain";
-  } else if (number === 2) {
-    prediction = "It is decidedly so";
-  } else if (number === 3) {
-    prediction = "Without a doubt";
-  } else if (number === 4) {
-    prediction = "Yes, definitely";
-  } else if (number === 5) {
-    prediction = "You may rely on it";
-  } else if (number === 6) {
-    prediction = "As I see it, yes";
-  } else if (number === 7) {
-    prediction = "Most likely";
-  } else if (number === 8) {
-    prediction = "Outlook good";
-  } else if (number === 9) {
-    prediction = "yes";
-  } else if (number === 10) {
-    prediction = "Signs point to yes";
-  } else if (number === 11 ) {
-    prediction = "Reply hazy try again";
-  } else if (number === 12) {
-    prediction = "Ask again later";
-  } else if (number === 13) {
-    prediction = "Better not tell you now";
-  } else if (number === 14) {
-    prediction = "Cannot predict now";
-  } else if (number === 15) {
-    prediction = "Concentrate and ask again";
-  } else if (number === 16) {
-    prediction = "Don't count on it";
-  } else if (number === 17) {
-    prediction = "My reply is no";
-  } else if (number === 18) {
-    prediction = "My sources say no";
-  } else if (number === 19) {
-    prediction = "Outlook not so good";
-  } else if (number === 20) {
-    prediction = "Very doubtful";
-  }
-  return prediction;
-};
+var obiAns = obiAnswer[Math.floor(Math.random()*obiAnswer.length)];
+var darthAns = darthAnswer[Math.floor(Math.random()*darthAnswer.length)];
+var yodaAns = yodaAnswer[Math.floor(Math.random()*yodaAnswer.length)];
 
-//Hello
+//Hello forever!!!
 
 // User Interface
 $(document).ready(function() {
-  $("form#ask").submit(function(event) {
+  $("form#obiSubmit").submit(function(event) {
     event.preventDefault();
     var question = $("input#inputtedQuestion").val();
-    var name = $("input#name").val();
-    var age = $("input#age").val();
 
-    var output = randomNumber();
-    $(".output").text(output);
-    console.log(output);
+    $(".output").text(obiAns);
     $("#ask").hide("slow");
     $("#result").slideDown("slow");
   });
+
+  $("form#darthSubmit").submit(function(event) {
+    event.preventDefault();
+    var question = $("input#inputtedQuestion").val();
+
+    $(".output").text(darthAns);
+    $("#ask").hide("slow");
+    $("#result").slideDown("slow");
+  });
+
+  $("form#yodaSubmit").submit(function(event) {
+    event.preventDefault();
+    var question = $("input#inputtedQuestion").val();
+
+    $(".output").text(yodaAns);
+    $("#ask").hide("slow");
+    $("#result").slideDown("slow");
+  });
+
+
+
 });
